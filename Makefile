LineageOS/android_kernel_xiaomi_msm8937

audio-kernel: Rename the techpack module to audio-legacy

Makefile
@@ -1,61 +1,61 @@
 # auto-detect subdirs
 ifeq ($(CONFIG_ARCH_SDM845),y)
-  audioconf := -include $(srctree)/techpack/audio/config/sdm845autoconf.h
-  include $(srctree)/techpack/audio/config/sdm845auto.conf
-  export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm845auto.conf)
+  audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm845autoconf.h
+  include $(srctree)/techpack/audio-legacy/config/sdm845auto.conf
+  export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm845auto.conf)
 else ifeq ($(CONFIG_ARCH_SDM670),y)
-  audioconf := -include $(srctree)/techpack/audio/config/sdm710autoconf.h
-  include $(srctree)/techpack/audio/config/sdm710auto.conf
-  export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm710auto.conf)
+  audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm710autoconf.h
+  include $(srctree)/techpack/audio-legacy/config/sdm710auto.conf
+  export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm710auto.conf)
 else
   # msm8953 platform
   ifeq ($(CONFIG_ARCH_SDM632),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_SDM450),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_SDM439),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_SDM429),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_MSM8953),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_MSM8940),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_MSM8937),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_MSM8917),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else
     # msm8909 platform
     ifeq ($(CONFIG_ARCH_QM215),y)
-      audioconf := -include $(srctree)/techpack/audio/config/msm8909autoconf.h
-      include $(srctree)/techpack/audio/config/msm8909auto.conf
-      export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/msm8909auto.conf)
+      audioconf := -include $(srctree)/techpack/audio-legacy/config/msm8909autoconf.h
+      include $(srctree)/techpack/audio-legacy/config/msm8909auto.conf
+      export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/msm8909auto.conf)
     else ifeq ($(CONFIG_ARCH_MSM8909),y)
-      audioconf := -include $(srctree)/techpack/audio/config/msm8909autoconf.h
-      include $(srctree)/techpack/audio/config/msm8909auto.conf
-      export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/msm8909auto.conf)
+      audioconf := -include $(srctree)/techpack/audio-legacy/config/msm8909autoconf.h
+      include $(srctree)/techpack/audio-legacy/config/msm8909auto.conf
+      export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/msm8909auto.conf)
     else
       ifeq ($(CONFIG_ARCH_SDXPOORWILLS),y)
-        audioconf := -include $(srctree)/techpack/audio/config/sdxpoorwillsautoconf.h
-        include $(srctree)/techpack/audio/config/sdxpoorwillsauto.conf
-        export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdxpoorwillsauto.conf)
+        audioconf := -include $(srctree)/techpack/audio-legacy/config/sdxpoorwillsautoconf.h
+        include $(srctree)/techpack/audio-legacy/config/sdxpoorwillsauto.conf
+        export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdxpoorwillsauto.conf)
       endif
     endif
   endif
@@ -63,13 +63,13 @@ endif
 
 # Use USERINCLUDE when you must reference the UAPI directories only.
 USERINCLUDE     += \
-                -I$(srctree)/techpack/audio/include/uapi \
+                -I$(srctree)/techpack/audio-legacy/include/uapi \
 
 # Use LINUXINCLUDE when you must reference the include/ directory.
 # Needed to be compatible with the O= option
 LINUXINCLUDE    += \
-                -I$(srctree)/techpack/audio/include/uapi \
-                -I$(srctree)/techpack/audio/include \
+                -I$(srctree)/techpack/audio-legacy/include/uapi \
+                -I$(srctree)/techpack/audio-legacy/include \
                 $(audioconf)
 
 obj-y += asoc/

Mi-Thorium/kernel_techpack_audio

Makefile
@@ -1,71 +1,71 @@
 # auto-detect subdirs
 ifeq ($(CONFIG_ARCH_SDXPOORWILLS),y)
-  audioconf := -include $(srctree)/techpack/audio/config/sdxpoorwillsautoconf.h
-  include $(srctree)/techpack/audio/config/sdxpoorwillsauto.conf
-  export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm710auto.conf)
+  audioconf := -include $(srctree)/techpack/audio-legacy/config/sdxpoorwillsautoconf.h
+  include $(srctree)/techpack/audio-legacy/config/sdxpoorwillsauto.conf
+  export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm710auto.conf)
 else ifeq ($(CONFIG_ARCH_SM8150),y)
-  audioconf := -include $(srctree)/techpack/audio/config/sm8150autoconf.h
-  include $(srctree)/techpack/audio/config/sm8150auto.conf
-  export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm710auto.conf)
+  audioconf := -include $(srctree)/techpack/audio-legacy/config/sm8150autoconf.h
+  include $(srctree)/techpack/audio-legacy/config/sm8150auto.conf
+  export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm710auto.conf)
 else ifeq ($(CONFIG_ARCH_SDMSHRIKE),y)
-  audioconf := -include $(srctree)/techpack/audio/config/sm8150autoconf.h
-  include $(srctree)/techpack/audio/config/sm8150auto.conf
-  export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm710auto.conf)
+  audioconf := -include $(srctree)/techpack/audio-legacy/config/sm8150autoconf.h
+  include $(srctree)/techpack/audio-legacy/config/sm8150auto.conf
+  export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm710auto.conf)
 else ifeq ($(CONFIG_ARCH_KONA),y)
-  audioconf := -include $(srctree)/techpack/audio/config/konaautoconf.h
-  include $(srctree)/techpack/audio/config/konaauto.conf
+  audioconf := -include $(srctree)/techpack/audio-legacy/config/konaautoconf.h
+  include $(srctree)/techpack/audio-legacy/config/konaauto.conf
 else ifeq ($(CONFIG_ARCH_SDM660),y)
-  audioconf := -include $(srctree)/techpack/audio/config/sdm660autoconf.h
-  include $(srctree)/techpack/audio/config/sdm660auto.conf
+  audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm660autoconf.h
+  include $(srctree)/techpack/audio-legacy/config/sdm660auto.conf
 else
   # msm8953 platform
   ifeq ($(CONFIG_ARCH_SDM632),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_SDM450),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_SDM439),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_SDM429),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_MSM8953),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_MSM8940),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_MSM8937),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else ifeq ($(CONFIG_ARCH_MSM8917),y)
-    audioconf := -include $(srctree)/techpack/audio/config/sdm450autoconf.h
-    include $(srctree)/techpack/audio/config/sdm450auto.conf
-    export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdm450auto.conf)
+    audioconf := -include $(srctree)/techpack/audio-legacy/config/sdm450autoconf.h
+    include $(srctree)/techpack/audio-legacy/config/sdm450auto.conf
+    export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdm450auto.conf)
   else
     # msm8909 platform
     ifeq ($(CONFIG_ARCH_QM215),y)
-      audioconf := -include $(srctree)/techpack/audio/config/msm8909autoconf.h
-      include $(srctree)/techpack/audio/config/msm8909auto.conf
-      export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/msm8909auto.conf)
+      audioconf := -include $(srctree)/techpack/audio-legacy/config/msm8909autoconf.h
+      include $(srctree)/techpack/audio-legacy/config/msm8909auto.conf
+      export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/msm8909auto.conf)
     else ifeq ($(CONFIG_ARCH_MSM8909),y)
-      audioconf := -include $(srctree)/techpack/audio/config/msm8909autoconf.h
-      include $(srctree)/techpack/audio/config/msm8909auto.conf
-      export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/msm8909auto.conf)
+      audioconf := -include $(srctree)/techpack/audio-legacy/config/msm8909autoconf.h
+      include $(srctree)/techpack/audio-legacy/config/msm8909auto.conf
+      export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/msm8909auto.conf)
     else
       ifeq ($(CONFIG_ARCH_SDXPOORWILLS),y)
-        audioconf := -include $(srctree)/techpack/audio/config/sdxpoorwillsautoconf.h
-        include $(srctree)/techpack/audio/config/sdxpoorwillsauto.conf
-        export $(shell sed 's/=.*//' $(srctree)/techpack/audio/config/sdxpoorwillsauto.conf)
+        audioconf := -include $(srctree)/techpack/audio-legacy/config/sdxpoorwillsautoconf.h
+        include $(srctree)/techpack/audio-legacy/config/sdxpoorwillsauto.conf
+        export $(shell sed 's/=.*//' $(srctree)/techpack/audio-legacy/config/sdxpoorwillsauto.conf)
       endif
     endif
   endif
@@ -73,14 +73,14 @@ endif
 
 # Use USERINCLUDE when you must reference the UAPI directories only.
 USERINCLUDE     += \
-                -I$(srctree)/techpack/audio/include/uapi \
-                -I$(srctree)/techpack/audio/include
+                -I$(srctree)/techpack/audio-legacy/include/uapi \
+                -I$(srctree)/techpack/audio-legacy/include
 
 # Use LINUXINCLUDE when you must reference the include/ directory.
 # Needed to be compatible with the O= option
 LINUXINCLUDE    += \
-                -I$(srctree)/techpack/audio/include/uapi \
-                -I$(srctree)/techpack/audio/include \
+                -I$(srctree)/techpack/audio-legacy/include/uapi \
+                -I$(srctree)/techpack/audio-legacy/include \
                 $(audioconf)
 
 obj-y += soc/